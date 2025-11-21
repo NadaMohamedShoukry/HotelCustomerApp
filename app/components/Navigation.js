9a0b1433801d9ea35242f56e89c3a28283d15f53
NadaMohamedShoukry <nadashoukry23@gmail.com>
2025-11-21 19:12:57 +0200

    initial project

---
+++ app/components/Navigation.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <ul>
+      <li>
+        <Link href="/cabins">Cabins</Link>
+      </li>
+    </ul>
+  );
+}
